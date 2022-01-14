bfdbb1d5ac57bfcd7d24a8800cacb9bbe8e644ff
jamesshaw-sf <85496350+jamesshaw-sf@users.noreply.github.com>
2022-01-14 15:23:33 -0500

    chore: testing git setup on local machine

--- restaurantour/lib/main.dart
+++ restaurantour/lib/main.dart
@@ -9,6 +9,7 @@ class Restaurantour extends StatelessWidget {
   // This widget is the root of your application.
   const Restaurantour({Key? key}) : super(key: key);
 
+  // Test comment
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
