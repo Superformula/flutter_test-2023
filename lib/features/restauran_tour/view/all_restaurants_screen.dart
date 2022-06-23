107a5e3b81bdb6dcd39f8e5e209a5e0eeccc4f39
Prashanth Vontela <prashanth.r.vontela@gmail.com>
2022-06-23 13:57:45 -0500

    added loading spinner in list screen

--- lib/features/restauran_tour/view/all_restaurants_screen.dart
+++ lib/features/restauran_tour/view/all_restaurants_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:restaurantour/app_routes/app_routes.dart';
 import 'package:restaurantour/features/restauran_tour/view/restaurant_row_item.dart';
 import 'package:restaurantour/features/restauran_tour/view_model/restaurants_view_model.dart';
+import 'package:restaurantour/theme/app_color.dart';
 
 class AllRestaurantsScreen extends StatelessWidget {
   const AllRestaurantsScreen({Key? key}) : super(key: key);
@@ -10,26 +11,18 @@ class AllRestaurantsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     RestaurantsViewModel viewModel = context.watch<RestaurantsViewModel>();
+    if (viewModel.loading) {
+      return Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(AppColors.primaryFill),
+        ),
+      );
+    }
     return ListView.builder(
       itemCount: viewModel.allRestaurants.length + 1,
       itemBuilder: (BuildContext context, int index) {
         if (index == viewModel.allRestaurants.length) {
-          if (viewModel.allRestaurants.length ==
-              viewModel.totalRestaurantsCount) {
-            return const IgnorePointer();
-          }
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
-            child: TextButton(
-              onPressed: () {
-                viewModel.loadMore();
-              },
-              child: Text(
-                "View More",
-                style: Theme.of(context).textTheme.button,
-              ),
-            ),
-          );
+          return _viewMoreButton(context, viewModel);
         }
         return RestaurantRowItem(
           restaurant: viewModel.allRestaurants[index],
@@ -39,6 +32,24 @@ class AllRestaurantsScreen extends StatelessWidget {
           },
         );
       },
+    );
+  }
+
+  Widget _viewMoreButton(BuildContext context, RestaurantsViewModel viewModel) {
+    if (viewModel.allRestaurants.length == viewModel.totalRestaurantsCount) {
+      return const IgnorePointer();
+    }
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+      child: TextButton(
+        onPressed: () {
+          viewModel.loadMore();
+        },
+        child: Text(
+          "View More",
+          style: Theme.of(context).textTheme.button,
+        ),
+      ),
     );
   }
 }
