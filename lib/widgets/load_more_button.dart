3bc4cb42e692d83f56108b6831a68d0d398f0090
Prashanth Vontela <prashanth.r.vontela@gmail.com>
2022-06-27 17:05:39 -0500

    removed commented code

--- lib/widgets/load_more_button.dart
+++ lib/widgets/load_more_button.dart
@@ -1,37 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:restaurantour/theme/app_color.dart';
 
-// class LoadMoreButton extends StatelessWidget {
-//   final VoidCallback onLoadMoreTap;
-//   final bool isLoading;
-//   const LoadMoreButton({
-//     Key? key,
-//     required this.onLoadMoreTap,
-//     required this.isLoading,
-//   }) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
-//       child: isLoading
-//           ? Center(
-//               child: CircularProgressIndicator(
-//                 valueColor:
-//                     AlwaysStoppedAnimation<Color>(AppColors.primaryFill),
-//               ),
-//             )
-//           : TextButton(
-//               onPressed: onLoadMoreTap,
-//               child: Text(
-//                 "View More",
-//                 style: Theme.of(context).textTheme.button,
-//               ),
-//             ),
-//     );
-//   }
-// }
-
 class LoadMoreButton extends StatefulWidget {
   final VoidCallback onLoadMoreTap;
 
