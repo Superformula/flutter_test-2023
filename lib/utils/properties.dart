112191f88cdbe01106d05363d4a71216b93659e9
venkatesh <venkatesh.seelam@gmail.com>
2022-07-12 16:04:21 -0400

    New properties file has been added

---
+++ lib/utils/properties.dart
@@ -0,0 +1,21 @@
+import "package:flutter/material.dart";
+
+// Define white Material color for the MaterialApp widget
+const MaterialColor white = MaterialColor(
+  0xFFFFFFFF,
+  <int, Color>{
+    50: Color(0xFFFFFFFF),
+    100: Color(0xFFFFFFFF),
+    200: Color(0xFFFFFFFF),
+    300: Color(0xFFFFFFFF),
+    400: Color(0xFFFFFFFF),
+    500: Color(0xFFFFFFFF),
+    600: Color(0xFFFFFFFF),
+    700: Color(0xFFFFFFFF),
+    800: Color(0xFFFFFFFF),
+    900: Color(0xFFFFFFFF),
+  },
+);
+
+//Define App title
+const String appTitle = "RestauranTour";
