b28ff34598107fc4f1fbdfd37432155e8e8d704e
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-02-14 19:55:53 -0400

    feat: Created the home view to show the tabs. I’m using MVVM.  BREAKING CHANGE: I need to remove the homePage with the Fetch button because I reached the limit from Yelp GraphQL API.

---
+++ lib/modules/home/views/home_view.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:restaurantour/common/constants.dart';
+import 'package:stacked/stacked.dart';
+
+import '../../restaurant/views/favorites_restaurants_list_view.dart';
+import '../../restaurant/views/restaurants_list_view.dart';
+import '../view_models/home_view_model.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 2);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder.reactive(
+      viewModelBuilder: () => HomeViewModel(context),
+      onViewModelReady: (HomeViewModel viewModel) => viewModel.ready(),
+      builder: (context, HomeViewModel viewModel, child) => Scaffold(
+        appBar: AppBar(
+          title: const Text(Constants.appName),
+          bottom: TabBar(
+            indicatorSize: TabBarIndicatorSize.label,
+            controller: _tabController,
+            tabAlignment: TabAlignment.center,
+            tabs: const [
+              Tab(
+                text: Constants.allrestaurants,
+              ),
+              Tab(
+                text: Constants.myFavorites,
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          controller: _tabController,
+          children: const [
+            RestaurantsListView(),
+            FavoritesRestaurantsListView()
+          ],
+        ),
+      ),
+    );
+  }
+}
