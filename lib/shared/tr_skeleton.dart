de643f11ffb8f5cb2714d605d2898eca95312f0d
Marce Sanchez <marcelo.sanchez.m@gmail.com>
2024-02-15 21:33:46 -0400

    feat: add shimmer for TrSkeleton loading effect
    - Integrate shimmer library to implement TrSkeleton loading skeleton.
    - Create TrSkeleton widget to enhance UI loading states with shimmer effects.

---
+++ lib/shared/tr_skeleton.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class TrSkeleton extends StatelessWidget {
+  const TrSkeleton({
+    Key? key,
+    required this.height,
+    required this.width,
+    this.marginBottom = 8.0,
+    this.borderRadius = 12.0,
+  }) : super(key: key);
+
+  final double height;
+  final double width;
+  final double marginBottom;
+  final double borderRadius;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: marginBottom),
+      width: width,
+      child: Shimmer.fromColors(
+        baseColor: Colors.grey.shade300,
+        highlightColor: Colors.grey.shade100,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(borderRadius),
+            color: Colors.white,
+          ),
+          height: height,
+          width: width,
+        ),
+      ),
+    );
+  }
+}
