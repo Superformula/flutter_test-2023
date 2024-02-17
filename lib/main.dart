93def46a5077a96400bc6837e92a9798fbe02a74
Jeilson Araujo <jeilsonaraujo@icloud.com>
2024-02-17 14:10:29 -0300

    feat: await for setupInjection, setup theme, and remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:restaurantour/core/rt_colors.dart';
 import 'package:restaurantour/core/inject.dart';
-import 'package:restaurantour/features/restaurant_list/restaurant_list_screen.dart';
-import 'package:restaurantour/repositories/yelp_repository.dart';
+import 'package:restaurantour/features/restaurants_list/restaurants_list_screen.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-void main() {
-  setupInjection();
-  runApp(const Restaurantour());
+void main() async {
+  await setupInjection();
+  runApp(const RestauranTour());
 }
 
-class Restaurantour extends StatelessWidget {
-  // This widget is the root of your application.
-  const Restaurantour({super.key});
+class RestauranTour extends StatelessWidget {
+  const RestauranTour({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'RestauranTour',
       debugShowCheckedModeBanner: false,
       localizationsDelegates: const [
         AppLocalizations.delegate,
@@ -25,43 +23,15 @@ class Restaurantour extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Restaurantour'),
-            ElevatedButton(
-              child: const Text('Fetch Restaurants'),
-              onPressed: () async {
-                final yelpRepo = inject<YelpRepository>();
-
-                try {
-                  final result = await yelpRepo.getRestaurants();
-                  if (result != null) {
-                    print('Fetched ${result.restaurants!.length} restaurants');
-                  } else {
-                    print('No restaurants fetched');
-                  }
-                } catch (e) {
-                  print('Failed to fetch restaurants: $e');
-                }
-              },
-            ),
-          ],
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        colorScheme: const ColorScheme.light(
+          primary: RTColors.primaryFill,
+          onSurface: RTColors.secondaryText,
+          background: RTColors.background,
         ),
       ),
+      home: RestaurantsListScreen.create(),
     );
   }
 }
