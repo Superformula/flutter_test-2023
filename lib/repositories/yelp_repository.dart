493c86b6724b019fab084c37956ff192caa12635
Jeilson Araujo <jeilsonaraujo@icloud.com>
2024-02-17 14:08:46 -0300

    feat: refactor delegate error handling responsibility to the view model

--- lib/repositories/yelp_repository.dart
+++ lib/repositories/yelp_repository.dart
@@ -45,15 +45,11 @@ class YelpRepository {
   /// }
   ///
   Future<RestaurantQueryResult?> getRestaurants({int offset = 0}) async {
-    try {
-      final response = await dio.post<Map<String, dynamic>>(
-        '/v3/graphql',
-        data: _getQuery(offset),
-      );
-      return RestaurantQueryResult.fromJson(response.data!['data']['search']);
-    } catch (e) {
-      return null;
-    }
+    final response = await dio.post<Map<String, dynamic>>(
+      '/v3/graphql',
+      data: _getQuery(offset),
+    );
+    return RestaurantQueryResult.fromJson(response.data!['data']['search']);
   }
 
   String _getQuery(int offset) {
