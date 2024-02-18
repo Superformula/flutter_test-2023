76c58a7cfbb56b1d2b1907a92faf9aff9a012038
ribeiro-gabriel <gabriel.ribeiro@acanto.ca>
2024-02-18 14:43:16 -0300

    feat(ui): add initial structure to RestaurantsPage

--- lib/app/ui/pages/restaurants_page.dart
+++ lib/app/ui/pages/restaurants_page.dart
@@ -8,11 +8,66 @@ class RestaurantsPage extends StatefulWidget {
 }
 
 class _RestaurantsPageState extends State<RestaurantsPage> {
+  final controller = PageController();
+  double currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(() {
+      setState(() {
+        currentPage = controller.page!;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('RestaurTour'), centerTitle: true, ),
-      body: const Column(),
+    final theme = Theme.of(context);
+
+    const tabs = <Widget>[
+      Tab(
+        text: 'All Restaurants',
+      ),
+      Tab(
+        text: 'My Favorites',
+      ),
+    ];
+
+    final appbar = AppBar(
+      shadowColor: Colors.black.withOpacity(0.2),
+      title: const Text('RestauranTour'),
+      toolbarHeight: 100,
+      bottom: TabBar(
+        tabAlignment: TabAlignment.center,
+        indicatorColor: Colors.black,
+        indicatorSize: TabBarIndicatorSize.label,
+        labelColor: Colors.black,
+        unselectedLabelColor: const Color(0xFF606060),
+        labelStyle: theme.textTheme.labelMedium,
+        unselectedLabelStyle: theme.textTheme.labelMedium?.copyWith(),
+        tabs: tabs,
+      ),
+    );
+    return DefaultTabController(
+      initialIndex: 0,
+      length: tabs.length,
+      child: Scaffold(
+        appBar: appbar,
+        body: TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text(
+                "It's cloudy here",
+                style: theme.textTheme.labelMedium,
+              ),
+            ),
+            const Center(
+              child: Text("It's rainy here"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
