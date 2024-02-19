fdbfd6f9b363dee365ad518a21eba16a9ca8f811
ribeiro-gabriel <gabriel.ribeiro@acanto.ca>
2024-02-19 13:02:12 -0300

    feat(error): add ErrorMessages

---
+++ lib/app/core/error/error_messages.dart
@@ -0,0 +1,4 @@
+abstract class ErrorMessages {
+  static const noInternetConnection = 'Unable to establish an internet connection';
+  static const serverException = 'Oops! We had an internal problem. Try again!';
+}
