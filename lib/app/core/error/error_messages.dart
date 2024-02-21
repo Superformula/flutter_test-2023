ac8113daf873a820358862de6b3f867e5a0af7a5
ribeiro-gabriel <gabriel.ribeiro@acanto.ca>
2024-02-21 09:05:28 -0300

    feat(error): add error messages

--- lib/app/core/error/error_messages.dart
+++ lib/app/core/error/error_messages.dart
@@ -1,6 +1,8 @@
 abstract class ErrorMessages {
   static const noInternetConnection = 'Unable to establish an internet connection';
   static const serverException = 'Oops! We had an internal problem. Try again!';
+  static const cacheException = 'Oops! We had an internal problem. Try again!';
+  static const unknownException = 'Oops! We had an unknown problem. Try again!';
   static const noRestaurantData = 'No restaurant data saved.';
 
   static String noDataForRestaurantId(String id) =>
