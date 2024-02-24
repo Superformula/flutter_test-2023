0585113d612827900f9b9d9279a38aa94ef4df8c
milezabaleta <milenazabaleta@icloud.com>
2024-02-23 22:45:48 -0300

    chore: fvm and environment variables configured

--- lib/repositories/yelp_repository.dart
+++ lib/repositories/yelp_repository.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:restaurantour/models/restaurant.dart';
 
-const _apiKey = '<PUT YOUR API KEY HERE>';
+const _apiKey = String.fromEnvironment('API_KEY');
 
 class YelpRepository {
   late Dio dio;
