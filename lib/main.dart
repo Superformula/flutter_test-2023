4cb2f623d7231a088d8416a29044f03af58776e6
ruanwiller <ruansilvx@gmail.com>
2024-03-01 20:50:06 -0300

    feature: add tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:restaurantour/data/repositories/yelp_repository.dart';
 import 'package:restaurantour/presentation/restaurantour.dart';
 import 'package:restaurantour/secrets.dart';
 
@@ -20,42 +19,7 @@ class RestaurantourApp extends StatelessWidget {
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Restaurantour'),
-            ElevatedButton(
-              child: const Text('Fetch Restaurants'),
-              onPressed: () async {
-                final yelpRepo = YelpRepository(apiKey: await Secrets.apiKey);
-
-                try {
-                  final result = await yelpRepo.getRestaurants();
-                  if (result != null) {
-                    print('Fetched ${result.restaurants!.length} restaurants');
-                  } else {
-                    print('No restaurants fetched');
-                  }
-                } catch (e) {
-                  print('Failed to fetch restaurants: $e');
-                }
-              },
-            ),
-          ],
-        ),
-      ),
+      home: const RestaurantourProvider(child: Restaurantour()),
     );
   }
 }
---
+++ lib/presentation/restaurantour.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:restaurantour/data/repositories/yelp_repository.dart';
+import 'package:restaurantour/presentation/restaurants_cubit.dart';
+import 'package:restaurantour/secrets.dart';
+
+class RestaurantourProvider extends StatelessWidget {
+  const RestaurantourProvider({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        RepositoryProvider(
+          create: (_) {
+            return YelpRepository(apiKey: Secrets.resolvedApiKey);
+          },
+        ),
+        BlocProvider(
+          create: (context) {
+            return RestaurantsCubit(context.read());
+          },
+        ),
+      ],
+      child: child,
+    );
+  }
+}
+
+class Restaurantour extends StatefulWidget {
+  const Restaurantour({Key? key}) : super(key: key);
+
+  @override
+  State<Restaurantour> createState() => _RestaurantourState();
+}
+
+class _RestaurantourState extends State<Restaurantour>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController = TabController(length: 2, vsync: this);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          'RestauranTour',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        toolbarHeight: 100,
+        bottom: TabBar(
+          controller: tabController,
+          labelColor: Colors.black,
+          indicatorColor: Colors.black,
+          tabAlignment: TabAlignment.center,
+          indicatorPadding: const EdgeInsets.symmetric(horizontal: 30),
+          tabs: const [
+            _Tab(label: 'All Restaurants'),
+            _Tab(label: 'My Favorites'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: const [
+          SizedBox(),
+          SizedBox(),
+        ],
+      ),
+    );
+  }
+}
+
+class _Tab extends StatelessWidget {
+  const _Tab({
+    Key? key,
+    required this.label,
+  }) : super(key: key);
+
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Text(label),
+    );
+  }
+}
