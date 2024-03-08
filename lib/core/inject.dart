7fde339f12cbfcbd6c4a4135bafdd2fa82b52adc
Decripter <jeilsonaraujo@icloud.com>
2024-03-08 11:55:25 -0300

    feat: setup environments prod, stage, and local

--- lib/core/inject.dart
+++ lib/core/inject.dart
@@ -5,6 +5,7 @@ import 'package:restaurantour/components/rt_components.dart';
 import 'package:restaurantour/core/interceptor.dart';
 import 'package:restaurantour/core/logger.dart';
 import 'package:restaurantour/core/routes.dart';
+import 'package:restaurantour/main.dart';
 import 'package:restaurantour/repositories/restaurant_repository.dart';
 import 'package:restaurantour/services/event_bus_service.dart';
 import 'package:restaurantour/services/favorite_service.dart';
@@ -15,9 +16,16 @@ GetIt inject = GetIt.instance;
 Future<void> setupInjection() async {
   RTLogger.i(message: 'Setup injection');
 
-  await dotenv.load(fileName: ".env");
+  String _apiKey = '';
+  const String fileName = kIsProd ? '.env' : '.env_stage';
 
-  final _apiKey = dotenv.env['API_KEY'];
+  try {
+    await dotenv.load(fileName: fileName);
+    _apiKey = dotenv.env['API_KEY'] ?? '';
+    if (_apiKey.isEmpty) RTLogger.w(message: 'API_KEY is empty');
+  } catch (exception) {
+    RTLogger.e(message: 'Fail to load API_KEY, looking for $fileName file', exception: exception);
+  }
 
   await inject.reset();
   SharedPreferences sharedPref = await SharedPreferences.getInstance();
--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,25 @@ import 'package:restaurantour/app.dart';
 import 'package:restaurantour/core/inject.dart';
 import 'package:restaurantour/core/logger.dart';
 
+const String kAppFlavor = String.fromEnvironment('app.flavor', defaultValue: "prod");
+
+const String kFlavorStage = "stage";
+const String kFlavorProd = "prod";
+const String kFlavorLocal = "local";
+
+const bool kIsStage = kAppFlavor == kFlavorStage;
+const bool kIsProd = kAppFlavor == kFlavorProd;
+const bool kIsLocal = kAppFlavor == kFlavorLocal;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  if (kIsStage) RTLogger.d(message: 'Running in Stage mode');
+  if (kIsProd) RTLogger.w(message: 'Running in Prod mode');
+  if (kIsLocal) RTLogger.i(message: 'Running in Local mode');
+
   await setupInjection();
+
   RTLogger.i(message: 'Start app');
   runApp(const App());
 }
