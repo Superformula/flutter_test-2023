9c41e9c9aee529ec61122294330f2cdac693c5cb
Jodamco <jpehh@hotmail.com>
2024-04-14 16:28:11 -0300

    refactor(yelp repository): refactor repository to get mock data due to api limits

--- lib/data/repositories/yelp_repository.dart
+++ lib/data/repositories/yelp_repository.dart
@@ -1,9 +1,17 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:restaurantour/data/models/restaurant.dart';
 
 const _apiKey =
     'X-Me22MBBw6lIYs9fV-ntllINQ_8rfQXuHojnDY8UxXUpuSR0zHY5TSaPmyKY8qwqirh2LWdew6yjgxPSk2eiPWA77TgJHXRC4En1JJ0Kn2ej0vNHBTsuDfLVKMZZnYx';
+
+class MockResponse {
+  final Map<String, dynamic> data;
+  MockResponse(this.data);
+}
 
 class YelpRepository {
   late Dio dio;
@@ -23,17 +31,87 @@ class YelpRepository {
 
   Future<RestaurantQueryResult?> getRestaurants({int offset = 0}) async {
     try {
-      final response = await dio.post<Map<String, dynamic>>(
-        '/v3/graphql',
-        data: _getQuery(offset),
-      );
+      final response = await mockRestaurantsResponse();
+
+      // dio.post<Map<String, dynamic>>(
+      //   '/v3/graphql',
+      //   data: _getQuery(offset),
+      // );
+
       return RestaurantQueryResult.fromJson(response.data!['data']['search']);
-    } catch (e) {
-      return null;
+    } on DioException catch (error) {
+      debugPrint(error.message);
+      rethrow;
     }
   }
 
+  Future<MockResponse> mockRestaurantsResponse() async {
+    await Future.delayed(const Duration(seconds: 2));
+    final jsonString =
+        await rootBundle.loadString('assets/yelp_restaurants_response.json');
+    return MockResponse(jsonDecode(jsonString));
+  }
+
+  Future<RestaurantQueryResult?> getRestaurantReviews({int offset = 0}) async {
+    try {
+      final response = await mockRestaurantReviewsResponse();
+
+      // dio.post<Map<String, dynamic>>(
+      //   '/v3/graphql',
+      //   data: _getReviewsQuery(offset),
+      // );
+
+      return RestaurantQueryResult.fromJson(response.data!['data']['search']);
+    } on DioException catch (error) {
+      debugPrint(error.message);
+      rethrow;
+    }
+  }
+
+  Future<MockResponse> mockRestaurantReviewsResponse() async {
+    await Future.delayed(const Duration(seconds: 2));
+    final jsonString =
+        await rootBundle.loadString('assets/yelp_reviews_response.json');
+    return MockResponse(jsonDecode(jsonString));
+  }
+
   String _getQuery(int offset) {
+    return '''
+query getRestaurants {
+  search(location: "Las Vegas", limit: 20, offset: $offset) {
+    total    
+    business {
+      id
+      name
+      price
+      rating
+      photos
+      reviews {
+        id
+        rating
+        user {
+          id
+          image_url
+          name
+        }
+      }
+      categories {
+        title
+        alias
+      }
+      hours {
+        is_open_now
+      }
+      location {
+        formatted_address
+      }
+    }
+  }
+  }
+''';
+  }
+
+  String _getReviewsQuery(int offset) {
     return '''
 query getRestaurants {
   search(location: "Las Vegas", limit: 20, offset: $offset) {
