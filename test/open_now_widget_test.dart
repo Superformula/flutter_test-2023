9a4b1be4e252150d7f192e188461580804d25531
luis lopez <llopez@elcatador.com>
2024-04-15 19:39:17 -0400

    widget test implemented

--- test/open_now_widget_test.dart
+++ test/open_now_widget_test.dart
@@ -1,12 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:restaurantour/widgets/my_divider_widget.dart';
 import 'package:restaurantour/widgets/opennow_widget.dart';
 
 void main() {
   testWidgets("Open now widget should always return a text",
       (widgetTester) async {
-    await widgetTester.pumpWidget(OpenNowWidget(isOpen: true));
+    await widgetTester.pumpWidget(OpenNowWidget(
+      isOpen: true,
+    ));
 
-    final textFinder = find.text('open');
+    final textFinder = find.byIcon(Icons.circle);
 
     expect(textFinder, findsWidgets);
   });
