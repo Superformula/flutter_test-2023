10324a0c76d9828870da19e53832169bf794edb6
Cindi Nishimoto <cindi_nishi@hotmail.com>
2024-04-16 13:27:40 -0300

    refactor: change main and move restauran tour app to other folder

---
+++ lib/restaurant_tour_app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:restaurantour/core/utils/utils.dart';
+import 'package:restaurantour/presentation/restaurant_detail/view/view.dart';
+import 'package:restaurantour/presentation/restaurant_list/view/view.dart';
+import 'package:restaurantour/presentation/splash/view/view.dart';
+
+class RestauranTourApp extends StatelessWidget {
+  const RestauranTourApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: Routes.splash,
+      routes: {
+        Routes.splash: (context) => const SplashPage(),
+        Routes.restaurantList: (context) => const RestaurantListPage(),
+        Routes.restaurantDetail: (context) => const RestaurantDetailPage(),
+      },
+    );
+  }
+}
