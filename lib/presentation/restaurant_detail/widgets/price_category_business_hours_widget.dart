4b0b88ae04dc638e56d2a5eab16fd7750e5aa081
Cindi Nishimoto <cindi_nishi@hotmail.com>
2024-04-16 13:46:25 -0300

    feat: price category business hours widget

---
+++ lib/presentation/restaurant_detail/widgets/price_category_business_hours_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:restaurantour/core/utils/utils.dart';
+import 'package:restaurantour/domain/entities/entities.dart';
+import 'package:restaurantour/presentation/restaurant_detail/mixins/restaurant_mixin.dart';
+import 'package:restaurantour/presentation/widgets/widgets.dart';
+
+class PriceCategoryBusinessHoursWidget extends StatelessWidget
+    with RestaurantMixin {
+  final RestaurantEntity restaurantEntity;
+  const PriceCategoryBusinessHoursWidget({
+    super.key,
+    required this.restaurantEntity,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text(
+              "${restaurantEntity.price} ${restaurantEntity.displayCategory}",
+              style: const TextStyle(
+                fontSize: 12,
+              ),
+            ),
+            const Spacer(),
+            showBusinessHoursStatus(
+              restaurantEntity.isOpen,
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            const BusinessHoursCircleStatusWidget(),
+          ],
+        ),
+        const SizedBox(
+          height: 24,
+        ),
+        const Divider(
+          color: AppColors.lightGray,
+        ),
+        const SizedBox(
+          height: 24,
+        ),
+      ],
+    );
+  }
+}
