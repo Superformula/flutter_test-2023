e5839d69ef1214a29be3b28bc6b350ce46bde9d7
Cindi Nishimoto <cindi_nishi@hotmail.com>
2024-04-16 13:41:02 -0300

    feat: add splash page, splash view and view for export

---
+++ lib/presentation/splash/view/splash_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:restaurantour/presentation/splash/view/view.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SplashView();
+  }
+}
---
+++ lib/presentation/splash/view/splash_view.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:restaurantour/core/utils/utils.dart';
+import 'package:restaurantour/presentation/restaurant_list/view/view.dart';
+import 'package:restaurantour/presentation/splash/widgets/widgets.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 3),
+      () {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (_) => const RestaurantListPage(),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      appBar: AppBar(
+        backgroundColor: AppColors.white,
+      ),
+      body: const Center(
+        child: SizedBox(
+          height: 500,
+          width: double.maxFinite,
+          child: SplashAnimationWidget(),
+        ),
+      ),
+    );
+  }
+}
