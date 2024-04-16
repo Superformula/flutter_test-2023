2994b943d16e1a2bd6c2731423130cff3c31cc2f
Cindi Nishimoto <cindi_nishi@hotmail.com>
2024-04-16 15:18:48 -0300

    chore: remove api key and add out.txt

--- lib/core/utils/constants/api_key.dart
+++ lib/core/utils/constants/api_key.dart
@@ -1,4 +1,4 @@
 class ApiKey {
   static String key =
-      'a12Vq2_i4gRnkBOKdR_zsKIpJYHZd6jfDjiGZgDk5K631QZR8hnCvuWnvYSz15bsxLz4B8EgdR4GRxe6P1KTton5r7JJhbBtaJVlEvsApNNlPdili3YYVfwuDKQeZnYx';
+      '';
 }
