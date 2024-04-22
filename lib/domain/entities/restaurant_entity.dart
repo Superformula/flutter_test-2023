7e426ced24a6f1710e616cc9c925be33069bbea3
Cindi Nishimoto <cindi_nishi@hotmail.com>
2024-04-22 12:07:18 -0300

    refactor: adjust restaurant entity to not use json serializable

--- lib/domain/entities/restaurant_entity.dart
+++ lib/domain/entities/restaurant_entity.dart
@@ -1,108 +1,14 @@
 import 'package:equatable/equatable.dart';
-import 'package:json_annotation/json_annotation.dart';
-part 'restaurant_entity.g.dart';
 
-@JsonSerializable()
-class CategoryEntity extends Equatable {
-  final String? alias;
-  final String? title;
-
-  const CategoryEntity({
-    this.alias,
-    this.title,
-  });
-
-  @override
-  List<Object?> get props => [alias, title];
-
-  factory CategoryEntity.fromJson(Map<String, dynamic> json) =>
-      _$CategoryEntityFromJson(json);
-
-  Map<String, dynamic> toJson() => _$CategoryEntityToJson(this);
-}
-
-@JsonSerializable()
-class HoursEntity extends Equatable {
-  final bool? isOpenNow;
-
-  const HoursEntity({
-    this.isOpenNow,
-  });
-
-  @override
-  List<Object?> get props => [isOpenNow];
-
-  factory HoursEntity.fromJson(Map<String, dynamic> json) =>
-      _$HoursEntityFromJson(json);
-
-  Map<String, dynamic> toJson() => _$HoursEntityToJson(this);
-}
-
-@JsonSerializable()
-class UserEntity extends Equatable {
-  final String? id;
-  final String? imageUrl;
-  final String? name;
-
-  const UserEntity({this.id, this.imageUrl, this.name});
-
-  @override
-  List<Object?> get props => [id, imageUrl, name];
-
-  factory UserEntity.fromJson(Map<String, dynamic> json) =>
-      _$UserEntityFromJson(json);
-
-  Map<String, dynamic> toJson() => _$UserEntityToJson(this);
-}
-
-@JsonSerializable()
-class ReviewEntity extends Equatable {
-  final String? id;
-  final int? rating;
-  final UserEntity? user;
-
-  const ReviewEntity({
-    this.id,
-    this.rating,
-    this.user,
-  });
-
-  @override
-  List<Object?> get props => [id, rating, user];
-
-  factory ReviewEntity.fromJson(Map<String, dynamic> json) =>
-      _$ReviewEntityFromJson(json);
-
-  Map<String, dynamic> toJson() => _$ReviewEntityToJson(this);
-}
-
-@JsonSerializable()
-class LocationEntity extends Equatable {
-  final String? formattedAddress;
-
-  const LocationEntity({
-    this.formattedAddress,
-  });
-
-  @override
-  List<Object?> get props => [formattedAddress];
-
-  factory LocationEntity.fromJson(Map<String, dynamic> json) =>
-      _$LocationEntityFromJson(json);
-
-  Map<String, dynamic> toJson() => _$LocationEntityToJson(this);
-}
-
-@JsonSerializable()
 class RestaurantEntity extends Equatable {
   final String? id;
   final String? name;
   final String? price;
   final double? rating;
   final List<String>? photos;
-  final List<CategoryEntity>? categories;
-  final List<HoursEntity>? hours;
   final List<ReviewEntity>? reviews;
+  final List<CategoriesEntity>? categories;
+  final List<HoursEntity>? hours;
   final LocationEntity? location;
 
   const RestaurantEntity({
@@ -111,31 +17,11 @@ class RestaurantEntity extends Equatable {
     this.price,
     this.rating,
     this.photos,
+    this.reviews,
     this.categories,
     this.hours,
-    this.reviews,
     this.location,
   });
-
-  @override
-  List<Object?> get props {
-    return [
-      id,
-      name,
-      price,
-      rating,
-      photos,
-      categories,
-      hours,
-      reviews,
-      location,
-    ];
-  }
-
-  factory RestaurantEntity.fromJson(Map<String, dynamic> json) =>
-      _$RestaurantEntityFromJson(json);
-
-  Map<String, dynamic> toJson() => _$RestaurantEntityToJson(this);
 
   /// Use the first category for the category shown to the userModel
   String get displayCategory {
@@ -161,9 +47,86 @@ class RestaurantEntity extends Equatable {
     }
     return false;
   }
+
+  @override
+  List<Object?> get props => [
+        id,
+        name,
+        price,
+        rating,
+        photos,
+        reviews,
+        categories,
+        hours,
+        location,
+      ];
 }
 
-@JsonSerializable()
+class CategoriesEntity extends Equatable {
+  final String? title;
+  final String? alias;
+
+  const CategoriesEntity({
+    this.title,
+    this.alias,
+  });
+
+  @override
+  List<Object?> get props => [title, alias];
+}
+
+class HoursEntity extends Equatable {
+  final bool? isOpenNow;
+
+  const HoursEntity({
+    this.isOpenNow,
+  });
+
+  @override
+  List<Object?> get props => [isOpenNow];
+}
+
+class LocationEntity extends Equatable {
+  final String? formattedAddress;
+
+  const LocationEntity({
+    this.formattedAddress,
+  });
+
+  @override
+  List<Object?> get props => [formattedAddress];
+}
+
+class ReviewEntity extends Equatable {
+  final String? id;
+  final int? rating;
+  final UserEntity? user;
+
+  const ReviewEntity({
+    this.id,
+    this.rating,
+    this.user,
+  });
+
+  @override
+  List<Object?> get props => [id, rating, user];
+}
+
+class UserEntity extends Equatable {
+  final String? id;
+  final String? imageUrl;
+  final String? name;
+
+  const UserEntity({
+    this.id,
+    this.imageUrl,
+    this.name,
+  });
+
+  @override
+  List<Object?> get props => [id, imageUrl, name];
+}
+
 class RestaurantQueryResultEntity extends Equatable {
   final int? total;
   final List<RestaurantEntity>? restaurants;
@@ -175,9 +138,4 @@ class RestaurantQueryResultEntity extends Equatable {
 
   @override
   List<Object?> get props => [total, restaurants];
-
-  factory RestaurantQueryResultEntity.fromJson(Map<String, dynamic> json) =>
-      _$RestaurantQueryResultEntityFromJson(json);
-
-  Map<String, dynamic> toJson() => _$RestaurantQueryResultEntityToJson(this);
 }
