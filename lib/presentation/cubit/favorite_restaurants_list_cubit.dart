7b7324da36c724087dc779cc1bc30bc8bf57133b
Cindi Nishimoto <cindi_nishi@hotmail.com>
2024-04-22 00:39:59 -0300

    refactor: delete old cubit files location

--- lib/presentation/cubit/favorite_restaurants_list_cubit.dart
+++ lib/presentation/cubit/favorite_restaurants_list_cubit.dart
@@ -2,14 +2,22 @@ import 'package:bloc/bloc.dart';
 import 'package:injectable/injectable.dart';
 import 'package:restaurantour/core/utils/status_enum.dart';
 import 'package:restaurantour/domain/entities/entities.dart';
-import 'package:restaurantour/presentation/restaurant_detail/cubit/cubit.dart';
+import 'package:restaurantour/presentation/cubit/cubit.dart';
 
 @singleton
 class FavoriteRestaurantsCubit extends Cubit<FavoriteRestaurantsListState> {
   final List<RestaurantEntity> _favoriteRestaurants = [];
 
-  FavoriteRestaurantsCubit()
-      : super(const FavoriteRestaurantsListState(restaurantsList: []));
+  FavoriteRestaurantsCubit() : super(_initialState());
+
+  static FavoriteRestaurantsListState _initialState() {
+    final List<RestaurantEntity> _favoriteRestaurants = [];
+    return FavoriteRestaurantsListState(
+      restaurantsList: const [],
+      status:
+          _favoriteRestaurants.isEmpty ? StatusEnum.empty : StatusEnum.success,
+    );
+  }
 
   void toggleFavorite(RestaurantEntity restaurant) {
     if (_favoriteRestaurants.contains(restaurant)) {
@@ -17,9 +25,12 @@ class FavoriteRestaurantsCubit extends Cubit<FavoriteRestaurantsListState> {
     } else {
       _favoriteRestaurants.add(restaurant);
     }
+
     emit(
       state.copyWith(
-        status: StatusEnum.success,
+        status: _favoriteRestaurants.isEmpty
+            ? StatusEnum.empty
+            : StatusEnum.success,
         restaurantsList: List<RestaurantEntity>.from(_favoriteRestaurants),
       ),
     );
