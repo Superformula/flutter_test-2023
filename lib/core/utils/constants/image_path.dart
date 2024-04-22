d5947405ebe8c8ec1eaf6d88c44a78279579c5a2
Cindi Nishimoto <cindi_nishi@hotmail.com>
2024-04-22 12:04:40 -0300

    refactor: change no photo user avatar path from asset to url

--- lib/core/utils/constants/image_path.dart
+++ lib/core/utils/constants/image_path.dart
@@ -1,5 +1,5 @@
 class ImagePath {
   static String chef = "assets/images/chef.png";
-  static String noPhotoUserAvatar = "assets/images/avatar.jpg";
+  static String noPhotoUserAvatar = "https://static.vecteezy.com/ti/vetor-gratis/p1/27448973-avatar-conta-icone-padrao-social-meios-de-comunicacao-perfil-foto-vetor.jpg";
   static String emptyState = "assets/images/empty.svg";
 }
