cee99f3c934d23c685c01c6c113ee59d11585a26
Salty Spaghetti <32353047+SaltySpaghetti@users.noreply.github.com>
2024-04-26 19:36:12 +0200

    chore: removed ApiKey

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,4 @@
 class AppConstants {
   static const String baseUrl = 'https://api.yelp.com/v3/graphql';
-  static const String apiKey =
-      'xOA-VVasGukJTuM854EGcRGT6WsOD852C5ikXy3wrgwEKdlw0nFWOmNP1LKEyF5HjOa8DD0BH6wKBM66AI9QbuT5_C2ZGhsALzztJ2-p0p97dXMp1oguTLOmAH4rZnYx';
+  static const String apiKey = '';
 }
