2517a457b0f66203c55b7b9f2e2d26667b1959d4
Salty Spaghetti <matteoterzuolo@gmail.com>
2024-04-26 17:23:52 +0200

    feat: updated YelpRepository

--- lib/di/repositories.dart
+++ lib/di/repositories.dart
@@ -1,4 +1,9 @@
 part of 'dependency_injector.dart';
 
 final List<RepositoryProvider> _repositories = [
+  RepositoryProvider<YelpRepository>(
+    create: (context) => YelpRepository(
+      service: context.read<YelpService>(),
+    ),
+  ),
 ];
