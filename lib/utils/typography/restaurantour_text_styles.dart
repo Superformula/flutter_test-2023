dbf5c54c555fa749dc3aaf0918fa31532acda97b
fonsecguilherme <guilherme.ramos@arapiraca.ufal.br>
2024-06-10 23:26:33 -0300

    feat: created app typography file

---
+++ lib/utils/typography/restaurantour_text_styles.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class RestaurantourTextStyles {
+  static const _loraFontFamily = 'Lora';
+  static const _openSansFontFamily = 'OpenSans';
+
+  static const TextStyle h6 = TextStyle(
+    fontFamily: _loraFontFamily,
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+
+  static const TextStyle subtitle1Lora = TextStyle(
+    fontFamily: _loraFontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+
+  static const TextStyle rating = TextStyle(
+    fontFamily: _loraFontFamily,
+    fontSize: 28,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+
+  static const TextStyle caption = TextStyle(
+    fontFamily: _openSansFontFamily,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+
+  static const TextStyle overline = TextStyle(
+    fontFamily: _openSansFontFamily,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+
+  static const TextStyle address = TextStyle(
+    fontFamily: _openSansFontFamily,
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  );
+
+  static const TextStyle body = TextStyle(
+    fontFamily: _openSansFontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+}
